e40051013671c8d4dd373e8bdfe37bf7677983e8
AmeerZedany <ameer.zedane2@gmail.com>
2025-04-02 17:06:21 +0300

    page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Nihad-web/',
+  base: '/Nihad-web/', // 👈 MUST match your GitHub repo name
   plugins: [react()],
 })
